7f407ed24acea2c51e49d93daf7cd7714cf8527d
Johannes Treitz <jotreitz@gmail.com>
2013-02-22 14:20:40 +0100

    Fix create_from_variant

--- lib/cornerstore/api/line_item.rb
+++ lib/cornerstore/api/line_item.rb
@@ -1,15 +1,15 @@
 class Cornerstore::LineItem < Cornerstore::Model::Base
   include Cornerstore::Model::Writable
-  
+
   attr_accessor :order_number,
                 :description,
                 :qty,
                 :unit,
                 :price,
                 :weight
-                
-  alias cart parent                
-                
+
+  alias cart parent
+
   validates :order_number, length: { within: 1..50 }
   validates :description, length: { within: 1..255 }
   validates :qty, numericality: { greater_than: 0, only_integer: true }
@@ -19,12 +19,12 @@ class Cornerstore::LineItem < Cornerstore::Model::Base
   validate do
     errors.add(:price, 'Price must be valid') unless price.valid?
   end
-  
+
   def initialize(attributes = {}, parent = nil)
     self.price = Cornerstore::Price.new(attributes.delete('price'))
     super
   end
-    
+
   def attributes
     {
       order_number: order_number,
@@ -35,17 +35,18 @@ class Cornerstore::LineItem < Cornerstore::Model::Base
       weight: weight
     }
   end
-  
+
   class Resource < Cornerstore::Resource::Base
     include Cornerstore::Resource::Remote
     include Cornerstore::Resource::Writable
-    
-    def create_from_variant(variant, attributes={})
-      attributes.merge!({
+
+    def create_from_variant(variant, attr={})
+      attributes = {
         variant_id: variant.id,
-        product_id: variant.product.id
-      })
-      response = RestClient.post("#{Cornerstore.root_url}/carts/#{@parent.id}/line_items/derive.json", attributes)  
+        product_id: variant.product.id,
+        line_item: attr
+      }
+      response = RestClient.post("#{Cornerstore.root_url}/carts/#{@parent.id}/line_items/derive.json", attributes)
       attributes = ActiveSupport::JSON.decode(response)
       line_item = @klass.new(attributes)
       push line_item
