869cabb01d4d198042a82ae8e2639cecdaa2fba7
Christian Weyer <caducitynights@gmail.com>
2013-11-14 12:47:12 +0100

    added comparison methods and float conversion

--- lib/cornerstore/api/price.rb
+++ lib/cornerstore/api/price.rb
@@ -7,7 +7,7 @@ class Cornerstore::Price < Cornerstore::Model::Base
   validates :gross, numericality: { greater_than_or_equal_to: 0 }
   validates :net, numericality: { greater_than_or_equal_to: 0 }
   validates :tax_rate, numericality: { greater_than_or_equal_to: 0 }
-  validates :currency, :presence => true
+  validates :currency, presence: true
                 
   def attributes
     {
@@ -16,5 +16,20 @@ class Cornerstore::Price < Cornerstore::Model::Base
       tax_rate: tax_rate,
       currency: currency
     }
+  end
+  
+  def <=> (other_object)
+    case other_object
+    when Integer, Float, Fixnum
+      other_object <=> self.gross
+    when Cornerstore::Price
+      other_object.gross <=> self.gross
+    else
+      raise ArgumentError, "can only compare Integer, Float, Fixnum or Price objects with Price"
+    end
+  end
+  
+  def to_f
+    self.gross
   end
 end
